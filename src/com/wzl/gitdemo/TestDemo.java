600e9f6507281b1197e35673fd07b93b17973069
WZL <2606024478@qq.com>
2022-09-22 19:59:34 +0800

    master third

--- src/com/wzl/gitdemo/TestDemo.java
+++ src/com/wzl/gitdemo/TestDemo.java
@@ -2,6 +2,6 @@ package com.wzl.gitdemo;
 
 public class TestDemo {
     public static void main(String[] args) {
-        System.out.println("master third");
+        System.out.println("master fourth");
     }
 }
